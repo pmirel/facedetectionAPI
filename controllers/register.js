b9e42fffd7c8734ef0aab6669c4c79afae2089b6
M <M@DESKTOP-J0D963F>
2020-08-28 21:35:09 +0300

    register

--- controllers/register.js
+++ controllers/register.js
@@ -1,14 +1,14 @@
 const handleRegister = (req, res, db, bcrypt) => {
-  const { email, password, name } = req.body;
-  if (!email || !password || !name) {
-    return res.status(400).json('incorect form submision')
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password)
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
       .into('login')
       .returning('email')
       .then(loginEmail => {
@@ -18,14 +18,15 @@ const handleRegister = (req, res, db, bcrypt) => {
             email: loginEmail[0],
             name: name,
             joined: new Date()
-          }).then(user => {
-            res.json(user[0])
+          })
+          .then(user => {
+            res.json(user[0]);
           })
       })
       .then(trx.commit)
       .catch(trx.rollback)
-  })
-    .catch(err => res.status(400).json('unable to register from server'))
+    })
+    .catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
