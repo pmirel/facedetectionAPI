94dda73790ab11802d20375e4b580171c35261b6
M <M@DESKTOP-J0D963F>
2020-08-28 13:47:06 +0300

    database url

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'facedetectionDB'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
